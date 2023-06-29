df22cabe3fbc827f93d3b4bfd3056def8cf66bf3
Vansh kumar <kumarvansh16aug@gmail.com>
2023-06-29 20:57:15 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //1D Array
+    // int arr[5];
+    // cout<<"Enter 5 numbers: ";
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // cout<<"the arr[3] :"<<arr[3];
+     
+
+    //2D Array
+    int arr2[2][3];  
+    cout<<"Enter 6 numbers: ";
+    cin>>arr2[0][0]>>arr2[0][1]>>arr2[0][2]>>arr2[1][0]>>arr2[1][1]>>arr2[1][2];
+    cout<<"the arr2[1][2] :"<<arr2[1][2];
+    return 0;
+}
